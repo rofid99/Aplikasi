24277fd10f4414ea31bb818bff447490cea6aef4
rofid99 <rofidsmk@gmail.com>
2021-05-17 20:30:24 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Ruang</title>
+    
+    <ul>
+    <li>D1L1</li>
+    <li>D2L1</li>
+    <li>D3L1</li>
+    <li>D4L1</li>
+    </ul>
+</head>
+<body>
+    
+</body>
+</html>
